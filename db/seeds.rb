e562dbae27594e9426cd618b46d615af07dce726
Francisco Mendonca <franciscomendonca8@gmail.com>
2022-02-21 19:25:58 +0000

    fixed html on index and associated card template

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Nft.destroy_all
-Nft.create(name: "Spaceship", description: "Lorem iupsum", price: 5, wallet_address: "ujsdfksdi")
+Nft.create(name: "Spaceship 1", description: "Lorem iupsum", price: 5, wallet_address: "ujsdfksdi")
+Nft.create(name: "Spaceship 2", description: "Some description", price: 5, wallet_address: "fasdfds")
+Nft.create(name: "Spaceship 3", description: "Another description", price: 5, wallet_address: "afsdfds")
 
 puts "Created #{Nft.count} nfts"
