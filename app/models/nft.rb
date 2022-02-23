ad1885c12a631bf8cf4950c6724a08786fb3a56d
Revdep <sbodezzi@gmail.com>
2022-02-23 12:28:37 +0100

    add validation to photo

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -2,4 +2,5 @@ class Nft < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
+  validates :photo, presence: true
 end
