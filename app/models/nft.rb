d22599f456d5ce658662c07b003e775ad9ed8c60
Francisco Mendonca <franciscomendonca8@gmail.com>
2022-02-23 15:27:47 +0000

    added seeds with photos and fixed responsiveness

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -4,5 +4,4 @@ class Nft < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
-  validates :photo, presence: true
 end
