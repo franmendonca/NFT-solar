94db815fc6f64001dcde32b97baf89be1db49af2
Revdep <sbodezzi@gmail.com>
2022-02-23 14:48:06 +0100

    implemented cloudinary

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -1,4 +1,6 @@
 class Nft < ApplicationRecord
+  has_one_attached :photo
+
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
